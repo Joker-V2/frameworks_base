652697ff923d7d9f8b6ad74d68905adf25461693
Joker-V2 <nipinna0@gmail.com>
2024-09-22 05:55:33 +0000

    Revert "Fix Photosphere/Camera FCs"
    This reverts commit dbc0fb4da90f3611152eec974e8beaf36cba49b1.

--- core/java/android/hardware/Camera.java
+++ core/java/android/hardware/Camera.java
@@ -3487,7 +3487,6 @@ public class Camera {
          * @see #getSceneMode()
          */
         public void setSceneMode(String value) {
-            if(getSupportedSceneModes() == null) return;
             set(KEY_SCENE_MODE, value);
         }
 
@@ -3525,7 +3524,6 @@ public class Camera {
          * @see #getFlashMode()
          */
         public void setFlashMode(String value) {
-	    if(getSupportedFlashModes() == null) return;
             set(KEY_FLASH_MODE, value);
         }
 
